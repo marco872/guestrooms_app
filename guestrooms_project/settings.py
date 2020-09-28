c1702e0646abac9ac791af8a34af22f30d5bb05c
marco872 <mrc.carrano@gmail.com>
2020-09-28 15:40:55 +0100

    more update

--- guestrooms_project/settings.py
+++ guestrooms_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*bk(rfrgidm+j%6bsy5b6*szn1gmzagvmane_%w7i5j4woq9+o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
